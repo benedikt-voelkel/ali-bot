04d16a7414966778e569101fe527d2ae92fbd3f8
Benedikt Volkel <benedikt.volkel@cern.ch>
2024-02-23 11:19:57 +0100

    Some change 113

--- daily-tags.sh
+++ daily-tags.sh
@@ -63,6 +63,10 @@ case $ARCHITECTURE in
   slc6*) PIP=pip PYTHON=python ;;
   *) PIP=pip3 PYTHON=python3 ;;
 esac
+
+
+
+
 # ...and override it if PYTHON_VERSION is specified.
 case "$PYTHON_VERSION" in
   2) PIP=pip2 PYTHON=python2 ;;
